43205ad7800d1b6bdb43067d3916a32e3cb4170e
tyakoske <nukubukatik@gmail.com>
2020-01-10 23:57:55 +0900

    ABC150 finish

--- ABC150/src/b_Count_ABC/Main.java
+++ ABC150/src/b_Count_ABC/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		 int n = sc.nextInt();
+		int n = sc.nextInt();
 		 String str = sc.nextLine();
 		 String s = sc.nextLine();
 		 sc.close();
